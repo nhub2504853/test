802422ab7f98bb9d8cd7d1178354706bf28dfe6a
nhub2504853 <nhub2504853@student.ctu.edu.vn>
2026-02-05 12:42:02 +0700

    First version

---
+++ hw1.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+    int T;
+    cin >> T;
+    while (T--) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << max(abs(x1-x2), abs(y1-y2)) << '\n';
+     }
+    }
